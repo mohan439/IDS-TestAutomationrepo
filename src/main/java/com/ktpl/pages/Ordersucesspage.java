2df4da98a8f1b4555bf1b388c208e88e15e706b8
mohan439 <mohannimmalapudi@gmail.com>
2020-09-03 11:26:09 +0530

    Update Ordersucesspage.java
    Removed the improper code

--- src/main/java/com/ktpl/pages/Ordersucesspage.java
+++ src/main/java/com/ktpl/pages/Ordersucesspage.java
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 package com.ktpl.pages;
 
 public class Ordersucesspage {
 
 }
-=======
 package com.ktpl.pages;
 
 public class Ordersucesspage {
 
 }
->>>>>>> 198e6a575fa48515f12a0c590d83f9d55a4fbe71
