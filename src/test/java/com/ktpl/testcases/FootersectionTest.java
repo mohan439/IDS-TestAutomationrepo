66c4d6344ed1dde9a122c97f222946a178bbfad0
mohan439 <mohannimmalapudi@gmail.com>
2020-09-03 11:30:26 +0530

    Update FootersectionTest.java
    Removed improper code

--- src/test/java/com/ktpl/testcases/FootersectionTest.java
+++ src/test/java/com/ktpl/testcases/FootersectionTest.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.ktpl.testcases;
 
 public class FootersectionTest {
 
 }
-=======
-package com.ktpl.testcases;
-
-public class FootersectionTest {
-
-}
->>>>>>> 198e6a575fa48515f12a0c590d83f9d55a4fbe71
