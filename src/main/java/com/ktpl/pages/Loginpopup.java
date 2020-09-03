d6195b5c20aaa548f4c41381a2ba6205a29629a3
mohan439 <mohannimmalapudi@gmail.com>
2020-09-03 11:24:14 +0530

    Update Loginpopup.java
    Removed improper code

--- src/main/java/com/ktpl/pages/Loginpopup.java
+++ src/main/java/com/ktpl/pages/Loginpopup.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ktpl.pages;
 
 import com.ktpl.parentbase.Baseinit;
@@ -8,14 +7,3 @@ public class Loginpopup extends Baseinit {
 	
 	
 }
-=======
-package com.ktpl.pages;
-
-import com.ktpl.parentbase.Baseinit;
-
-public class Loginpopup extends Baseinit {
-
-	
-	
-}
->>>>>>> 198e6a575fa48515f12a0c590d83f9d55a4fbe71
