a68bbf3a0971b87af2ebd66644308214c12669ab
mohan439 <mohannimmalapudi@gmail.com>
2020-09-03 11:14:12 +0530

    Update Forgotpasswordpage.java
    removed improper code

--- src/main/java/com/ktpl/pages/Forgotpasswordpage.java
+++ src/main/java/com/ktpl/pages/Forgotpasswordpage.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ktpl.pages;
 
 
@@ -12,18 +11,3 @@ public class Forgotpasswordpage extends Baseinit{
 		return driver.getTitle();
 	}
 }
-=======
-package com.ktpl.pages;
-
-
-import com.ktpl.parentbase.Baseinit;
-
-public class Forgotpasswordpage extends Baseinit{
-	
-	
-
-	public String Forgotpasswordpagetitle() {
-		return driver.getTitle();
-	}
-}
->>>>>>> 198e6a575fa48515f12a0c590d83f9d55a4fbe71
