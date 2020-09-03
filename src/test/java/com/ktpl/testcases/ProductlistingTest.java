d95482da34f92626e69524ac6146b9ee065d10a3
mohan439 <mohannimmalapudi@gmail.com>
2020-09-03 11:36:53 +0530

    Update ProductlistingTest.java
    Removed improper code

--- src/test/java/com/ktpl/testcases/ProductlistingTest.java
+++ src/test/java/com/ktpl/testcases/ProductlistingTest.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.ktpl.testcases;
 
 public class ProductlistingTest {
 
 }
-=======
-package com.ktpl.testcases;
-
-public class ProductlistingTest {
-
-}
->>>>>>> 198e6a575fa48515f12a0c590d83f9d55a4fbe71
