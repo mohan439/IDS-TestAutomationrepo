c337ade83c5b5f9beb60a761e10cb8d9cb324a49
mohan439 <mohannimmalapudi@gmail.com>
2020-09-03 12:13:53 +0530

    Update UtilityMethods.java
    Removed improper code and updated the paths

--- src/main/java/com/ktpl/utilities/UtilityMethods.java
+++ src/main/java/com/ktpl/utilities/UtilityMethods.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ktpl.utilities;
 
 import java.io.File;
@@ -67,7 +66,7 @@ public class UtilityMethods extends Baseinit {
 	public static void takescreenshot() {
 		TakesScreenshot screenshot = (TakesScreenshot)driver;
 		File source = screenshot.getScreenshotAs(OutputType.FILE);
-		String screenshotpath = "D://Selenium//Magento_space//FashiongoTestsuite//screenshots//"+System.currentTimeMillis()+".png";
+		String screenshotpath = System.getProperty("user.dir")+"//Reprots//screenshot"+System.currentTimeMillis()+".png";
 			
 		
 
@@ -78,84 +77,3 @@ public class UtilityMethods extends Baseinit {
 	// Switch frame
 
 }
-=======
-package com.ktpl.utilities;
-
-import java.io.File;
-
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.testng.ITestResult;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-import com.ktpl.pages.Ageverificationpopup;
-import com.ktpl.parentbase.Baseinit;
-
-public class UtilityMethods extends Baseinit {
-	
-
-	// Read data from excel file
-
-	// Extent report
-	
-	//Close newsletter pop up
-	public static void Entersite() {
-		Ageverificationpopup Agepopup = new Ageverificationpopup();
-		try {
-			Agepopup.selectcountry();
-		System.out.println("country selected");
-		//Agepopup.agecheckbox();
-		Agepopup.Entersite();
-		}catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("popup not displayed");
-		}
-		
-	}
-	public static ExtentReports extentreports() {
-		if(extent==null) {
-		
-			System.out.println("before test extent reprots");
-			htmlReporter = new ExtentHtmlReporter("./Reprots/Automationreport"+System.currentTimeMillis()+".html");
-			htmlReporter.config().setDocumentTitle("IDS Automation Report");
-			htmlReporter.config().setReportName("IDS Test Automation Reprot");
-			htmlReporter.config().setTheme(Theme.DARK);
-
-			extent = new ExtentReports();
-			extent.attachReporter(htmlReporter);
-		}
-		return extent;
-	}
-	
-	
-	public static void extentteardown(ITestResult result) {
-		System.out.println("after method listner");
-		if (result.getStatus() == ITestResult.FAILURE) {
-			test.log(Status.FAIL, result.getName() + "Testcase failed");
-			test.log(Status.FAIL, "Testcase failed" + result.getThrowable());
-		} else if (result.getStatus() == ITestResult.SUCCESS) {
-			test.log(Status.PASS, "Test case passed" + result.getName());
-		} else if (result.getStatus() == ITestResult.SKIP) {
-			test.log(Status.SKIP, "TestCase skipped" + result.getName());
-		}
-		
-	}
-
-	// Take screenshot
-	public static void takescreenshot() {
-		TakesScreenshot screenshot = (TakesScreenshot)driver;
-		File source = screenshot.getScreenshotAs(OutputType.FILE);
-		String screenshotpath = "D://Selenium//Magento_space//FashiongoTestsuite//screenshots//"+System.currentTimeMillis()+".png";
-			
-		
-
-
-	}
-	// Mouse hover
-
-	// Switch frame
-
-}
->>>>>>> 198e6a575fa48515f12a0c590d83f9d55a4fbe71
